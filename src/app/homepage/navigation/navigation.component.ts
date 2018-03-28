5d5da3f1ff9fc40d3da880835c24ff653d76fdff
Isabel M. Arias Salas <isabelminerva@outlook.com>
2018-03-28 17:31:02 -0400

    Navbar Menu switch between users navigation paths and if its logged in or not

--- src/app/homepage/navigation/navigation.component.ts
+++ src/app/homepage/navigation/navigation.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  userLoggedIn // this tells me if there is an user or if there isn't
 
+  client = [{name: 'home', link: '/home'},{name: 'home', link: '/home'},{name: 'home', link: '/home'}]
+  manager
+  mechanic
+  admin
+  navs = [this.client,this.mechanic, this.manager, this.admin]
+  menu
   constructor() { }
 
   ngOnInit() {
+    this.userLoggedIn = true; 
+    this.menu = this.navs[0] // so basically usertype - 1
   }
 
 }
