0cb87cfd589555b352002954dfcd56add2f418db
Isabel M. Arias Salas <isabelminerva@outlook.com>
2018-03-31 10:56:02 -0400

    Appointment design started

--- src/app/client/app/app.component.ts
+++ src/app/client/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, FormBuilder, NgModel, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'clientapp-app',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class ClientAppComponent implements OnInit {
-
-  constructor() { }
+  app
+  constructor(fb: FormBuilder) {
+    this.app = fb.group({
+      'date': [null, Validators.required]
+    })
+   }
 
   ngOnInit() {
   }
+
 
 }
